bc32034ae6acfd0846ab999610018fd5748f33cb
sya-hid <syarifhidayatullah2020@gmail.com>
2022-02-02 23:23:27 +0700

    update device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:delicious_food/HomePage.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+void main() => runApp(
+      DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => MyApp(), // Wrap your app
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
